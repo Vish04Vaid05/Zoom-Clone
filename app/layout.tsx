0637a1306a646e11fb282fc849e36c21e5aed847
Vish04Vaid05 <vishwask.vaidya@gmail.com>
2024-05-05 19:08:20 +0530

    Chages Made to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import '@stream-io/video-react-sdk/dist/css/styles.css';
 import "react-datepicker/dist/react-datepicker.css";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Zoom 2.0",
   description: "App created by Vishwas Kanchan Vaidya",
-  icons:{
+  icons: {
     icon: '/icons/logo.svg'
   }
 };
@@ -23,27 +23,38 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <head>
-        <link rel="icon" href="/icons/logo.svg"/>
+      <head>
+        <link rel="icon" href="/icons/logo.svg" />
+
+        {/* Open Graph Metadata for LinkedIn and Social Media */}
+        <meta property="og:title" content="Zoom 2.0" />
+        <meta property="og:description" content="App created by Vishwas Kanchan Vaidya" />
+        <meta property="og:image" content="/icons/logo.svg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://zoom-clone-b3hjx4sgi-vishwas-vaidyas-projects.vercel.app/" />
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       </head>
-      <ClerkProvider appearance={{
-        layout:{
-          logoImageUrl:'/icons/logo.svg',
-          socialButtonsVariant:'iconButton'
-        },
-        variables:{
-          colorText:"#fff",
-          colorPrimary:"#0E78F9",
-          colorBackground:"#1c1f2e",
-          colorInputBackground:"#252a41",
-          colorInputText:"#fff"
-        }
-      }}>
-      <body className={`${inter.className} bg-dark-2`}>
-        {children}
-        <Toaster/>
+      <ClerkProvider
+        appearance={{
+          layout: {
+            logoImageUrl: "/icons/logo.svg",
+            socialButtonsVariant: "iconButton",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
         </body>
-        </ClerkProvider>
+      </ClerkProvider>
     </html>
   );
 }
